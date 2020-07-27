4d3c6cb5eebe7fddc5fae74be6940cb0b674f93a
wil-bert <ianwilbuts@gmail.com>
2020-07-27 19:27:00 +0300

    Add hover on portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -29,13 +29,28 @@ $(document).ready(function () {
         $("#p-icon").toggle();
         $(".product").toggle();
     });
+$(document).ready(function(){
+    $(".paragraph").css('display', 'none');
+    var works =["work1", "work2", "work3", "work4", "work5", "work6", "work7", "work8"];
+    works.forEach(function(work){
+        $("." + work).hover(function(){
+            $(".paragraph",this).fadeIn()
+            $("." + work).css('opacity','0.5');
+            },
+        function (){
+            $(".paragraph").fadeOut()
+            $("." + work).css('opacity','1')
+        });
+        });
+    });
 });
 
 function submitx(){
 var name = document.getElementById("name").value;
 var email = document.getElementById("email").value;
+var message = document.getElementById("message").value;
 
-if (name.length !== 0 && email.length !== 0){
+if (name.length !== 0 && email.length !== 0 && message.length !== 0){
     alert(name + " Thank you for reaching out!");
 }
 else{
