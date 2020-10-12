6829cad2fe09a8c9d69df6ca66df19e457238945
Glo18 <kiogoraspecial@gmail.com>
2020-10-12 03:02:14 +0300

    modify html

--- Documents/moringa-school-projects/IP3/Delani-Studio/js/scripts.js
+++ Documents/moringa-school-projects/IP3/Delani-Studio/js/scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $(.WHAT WE DO)
-})
+$(document).ready(function() {
+    $(".onCrack1").click(function() {
+        $("#click1").toggle();
+    });
+    $(".onCrack2").click(function() {
+        $("#click2").toggle();
+    });
+    $(".onCrack3").click(function() {
+        $("#click3").toggle();
+    });
+});
