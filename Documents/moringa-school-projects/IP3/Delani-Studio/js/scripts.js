d3ff189c36aaad2113220e5573946de9feca5da6
Glo18 <kiogoraspecial@gmail.com>
2020-10-12 09:53:15 +0300

    make changes

--- Documents/moringa-school-projects/IP3/Delani-Studio/js/scripts.js
+++ Documents/moringa-school-projects/IP3/Delani-Studio/js/scripts.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-    $(".onCrack1").click(function() {
-        $("#click1").toggle();
-    });
-    $(".onCrack2").click(function() {
-        $("#click2").toggle();
-    });
-    $(".onCrack3").click(function() {
-        $("#click3").toggle();
-    });
-});
+$(document).ready(function(){
+    $(.WHAT WE DO)
+})
